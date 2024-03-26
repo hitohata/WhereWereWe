f0ca5e9f7be008fffe31f5a45bd75ef6220fdac7
hirohata <hirohatatro@gmail.com>
2024-03-25 23:18:21 -0700

    feat: added username

--- backend/core-where-were-we/app/users/src/models/user.rs
+++ backend/core-where-were-we/app/users/src/models/user.rs
@@ -1,5 +1,8 @@
+//! user's model
+use crate::errors::errors::UsersError;
 use crate::models::user_id::UserId;
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 struct User {
     /// User ID
     id: UserId,
@@ -13,7 +16,7 @@ impl User {
         Self {
             id: id.to_owned(),
             name: name.to_owned(),
-            email: name.to_string()
+            email: email.to_string()
         }
     }
 
@@ -25,16 +28,53 @@ impl User {
 /// Username must be grater than 0 and less than equal 255.
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct Username {
+    // must be 0 < name <= 255
     name: String
 }
 
 impl Username {
-    // TODO: implement validation
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: name.to_string()
+
+    pub fn name(&self) -> String {
+        (&self.name).to_string()
+    }
+
+    // the username must be 0 < name <= 255.
+    pub fn new(name: &str) -> Result<Self, UsersError> {
+        let name_len = name.len();
+
+        if 255 < name_len {
+            Err(UsersError::UsernameError("The name length must be less than 255 characters.".to_string()))
+        } else if name_len < 1 {
+            Err(UsersError::UsernameError("The name length must be grater than 0.".to_string()))
+        } else {
+            Ok(Self {
+                name: name.to_string()
+            })
         }
     }
 }
 
-// #[cfg(test)]
+#[cfg(test)]
+mod username_test {
+    use super::*;
+
+    #[test]
+    fn test_new_name() {
+        let name = Username::new("name").unwrap().name;
+        assert_eq!(name, "name");
+    }
+
+    #[test]
+    fn test_name_too_short() {
+        let name = Username::new("");
+        assert!(name.is_err());
+        assert_eq!(name.unwrap_err().to_string(), "[UsernameError]: The name length must be grater than 0.".to_string());
+    }
+
+    #[test]
+    fn test_name_too_long() {
+        let name = Username::new(&vec!["a"; 256].join(""));
+        assert!(name.is_err());
+        assert_eq!(name.unwrap_err().to_string(), "[UsernameError]: The name length must be less than 255 characters.".to_string());
+    }
+}
