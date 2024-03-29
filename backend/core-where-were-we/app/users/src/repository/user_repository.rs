adabbbac4f6d8206ea93ee1f9cd7e160e4a4014f
hirohata <hirohatatro@gmail.com>
2024-03-29 16:37:06 -0700

    feat: added table test

--- backend/core-where-were-we/app/users/src/repository/user_repository.rs
+++ backend/core-where-were-we/app/users/src/repository/user_repository.rs
@@ -6,31 +6,29 @@ use crate::models::user::{User, Username};
 use crate::models::user_id::UserId;
 use utils::settings::settings::table_name;
 
-struct UserRepositoyConcrete {
+#[derive(Debug)]
+struct UserRepositoryConcrete {
     client: aws_sdk_dynamodb::Client,
     table_name: String
 }
 
-impl UserRepositoyConcrete {
-    pub async fn new(client: Option<aws_sdk_dynamodb::Client>) -> Self {
+impl UserRepositoryConcrete {
+    async fn default() -> Self {
         let table_name = table_name().to_string();
-        match client {
-            Some(c) => Self { client: c, table_name },
-            None => Self { client: dynamodb_client().await, table_name }
-        }
+        Self { client: dynamodb_client().await, table_name }
     }
 }
 
-impl UserRepository for UserRepositoyConcrete {
+impl UserRepository for UserRepositoryConcrete {
     async fn find_by_id(&self, user_id: &UserId) -> Result<Option<User>> {
 
         let result = self.client.get_item()
             .table_name(&self.table_name)
             .key("PK", AttributeValue::S(user_id.id.to_string()))
-            .key("SK", AttributeValue::S("User".to_string()))
+            .key("SK", AttributeValue::S("USER".to_string()))
             .send()
             .await?;
-
+        
         let item = match result.item {
             Some(item) => item,
             None =>  return Ok(None)
@@ -88,8 +86,8 @@ impl UserRepository for UserRepositoyConcrete {
         let name_av = AttributeValue::S(user.name.name.to_string());
         let email_av = AttributeValue::S(user.email.to_string());
         let partners_av = AttributeValue::L(user.partners.iter().map(|p| AttributeValue::S(p.id.to_string())).collect::<Vec<AttributeValue>>());
-        
-        self.client
+
+        let res = self.client
             .put_item()
             .table_name(&self.table_name)
             .item("PK", user_id_av)
@@ -107,13 +105,67 @@ impl UserRepository for UserRepositoyConcrete {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::models::user::User;
     use crate::models::user_id::UserId;
-    
+    use tokio;
+    use test_utils::infrastructure::db::dynamo_db_client::{dynamodb_test_client, TestDynamoTable};
+
     // create username
     fn test_user() -> User {
         let user_id: UserId = UserId::try_from("62cba1c3-1d87-444e-8784-4ed43de4e79a").unwrap();
         let username: Username = Username::try_from("name").unwrap();
         User::new(&user_id, &username,  &"e@a", None)
+    }
+
+    impl UserRepositoryConcrete {
+        async fn new(table_name: &str) -> Self {
+            Self {
+                client: dynamodb_test_client().await,
+                table_name: table_name.to_string()
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_save_user() {
+        // Arrange
+        let table_name = "user_repository_save";
+        let repository = UserRepositoryConcrete::new(table_name).await;
+        let test_table = TestDynamoTable::default(table_name).await;
+        let user = test_user();
+        
+        // create table
+        test_table.generate_test_table().await;
+        
+        // Act
+        let _ = repository.save(&user).await; // save user
+
+        // Assert
+        assert!(true);
+
+        test_table.delete_table().await;
+    }
+    
+    #[tokio::test]
+    async fn test_find_user_by_id() {
+        // Arrange
+        let table_name = "user-repository-find-by-id";
+        let repository = UserRepositoryConcrete::new(table_name).await;
+        let test_table = TestDynamoTable::default(table_name).await;
+        let user = test_user();
+        
+        // create table
+        test_table.generate_test_table().await;
+        
+        // Act
+        let _ = repository.save(&user).await; // save user
+        let retrieved_user = repository.find_by_id(&user.id).await.unwrap();
+        
+        // Assert
+        match retrieved_user {
+            Some(retrieved) => assert!(retrieved.eq(&user)),
+            None => assert!(false)
+        }
+
+        test_table.delete_table().await;
     }
 }
