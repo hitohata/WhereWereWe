8d96aad6d8a29779a11227f4609ee158d471eded
hirohata <hirohatatro@gmail.com>
2024-05-13 15:39:28 -0700

    feat: default

--- backend/core-where-were-we/app/travel/src/service/todo_id_service.rs
+++ backend/core-where-were-we/app/travel/src/service/todo_id_service.rs
@@ -1,5 +1,7 @@
 //! Getting id service
 //! Return a number that latest ID + 1
+use utils::infrastructure::db::dynamo_db_client::dynamodb_client;
+use utils::settings::settings::table_name;
 use crate::errors::errors::TravelError;
 use crate::models::todo::id::todo_list_group_id::TodoListGroupId;
 use crate::models::travel::entity::travel::Travel;
@@ -8,4 +10,20 @@ use crate::models::travel::id::travel_id::TravelId;
 pub trait TodoIdService {
     async fn get_todo_list_group_id(travel_id: &TravelId) -> Result<u32, TravelError>;
     async fn get_todo_id(travel_id: &Travel, todo_id: &TodoListGroupId) -> Result<u32, TravelError>;
+}
+
+#[derive(Debug, Default)]
+pub struct TodoIdServiceConcrete {
+    client: aws_sdk_dynamodb::Client,
+    table_name: String
+}
+
+impl Default for TodoIdServiceConcrete {
+    async fn default() -> Self {
+        let table_name = table_name().to_string();
+        Self {
+            client: dynamodb_client().await,
+            table_name
+        }
+    }
 }
