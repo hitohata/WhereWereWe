a644a9cbdddeb8568e644e49439be12e644525f1
hitohata <43802663+hitohata@users.noreply.github.com>
2024-05-13 01:47:33 +0000

    Deploying to gh-pages from @ hitohata/WhereWereWe@fcdec81dc441f16cade0c1b08b086da5a0eecfbe 🚀

--- core-where-were-we/trait.impl/core/error/trait.Error.js
+++ core-where-were-we/trait.impl/core/error/trait.Error.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"travel":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"travel/errors/errors/enum.TravelError.html\" title=\"enum travel::errors::errors::TravelError\">TravelError</a>"]],
-"users":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"users/errors/errors/enum.UsersError.html\" title=\"enum users::errors::errors::UsersError\">UsersError</a>"]]
+"travel":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"travel/errors/errors/enum.TravelError.html\" title=\"enum travel::errors::errors::TravelError\">TravelError</a>"]],
+"users":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"users/errors/errors/enum.UsersError.html\" title=\"enum users::errors::errors::UsersError\">UsersError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
