1d26105828512bef115968fbf61ebcfa47924a97
hirohata <hirohatatro@gmail.com>
2024-05-20 19:46:24 -0700

    feat: added the get todo function

--- backend/core-where-were-we/app/travel/src/use_cases/todo_use_cases.rs
+++ backend/core-where-were-we/app/travel/src/use_cases/todo_use_cases.rs
@@ -2,6 +2,7 @@ use crate::dtos::todo::{ToDoDto, ToDoListGroupDto};
 use crate::errors::errors::TravelError;
 use crate::models::repository::todo_repository::TodoRepository;
 use crate::models::repository::travel_repository::TravelRepository;
+use crate::models::todo::id::todo_id::TodoId;
 use crate::models::todo::id::todo_list_group_id::TodoListGroupId;
 use crate::models::travel::id::travel_id::TravelId;
 
@@ -45,10 +46,7 @@ impl<R, RP> ToDoUseCases for TodoUseCaseInstractor<R, RP>
 {
     async fn travel_to_do_list_group(&self, travel_id: &str) -> Result<Vec<ToDoListGroupDto>, TravelError> {
         
-        let travel_id_struct = match TravelId::try_from(travel_id) {
-            Ok(t_id) => t_id,
-            Err(e) => return Err(e)
-        };
+        let travel_id_struct = TravelId::try_from(travel_id)?;
         
         match self.todo_repository.list_todo_list_group(&travel_id_struct).await {
             Ok(todo_list_group) => Ok(todo_list_group.iter().map(|el| ToDoListGroupDto::from(el)).collect()),
@@ -57,10 +55,7 @@ impl<R, RP> ToDoUseCases for TodoUseCaseInstractor<R, RP>
     }
 
     async fn get_todo_list_group(&self, travel_id: &str, to_do_list_group_id: &u32) -> Result<Option<ToDoListGroupDto>, TravelError> {
-        let travel_id_struct = match TravelId::try_from(travel_id) {
-            Ok(t) => t,
-            Err(e) => return Err(e)
-        };
+        let travel_id_struct = TravelId::try_from(travel_id)?;
         
         let todo_list_group_id = TodoListGroupId::from(to_do_list_group_id);
         
@@ -76,7 +71,20 @@ impl<R, RP> ToDoUseCases for TodoUseCaseInstractor<R, RP>
     }
 
     async fn get_todo(&self, travel_id: &str, todo_list_group_id: &u32, todo_id: &u32) -> Result<Option<ToDoDto>, TravelError> {
-        todo!()
+        let travel_id_struct = TravelId::try_from(travel_id)?;
+        let todo_list_group_id = TodoListGroupId::from(todo_list_group_id);
+        let todo_id = TodoId::from(todo_id);
+        
+        match self.todo_repository.find_todo_by_id(&travel_id_struct, &todo_list_group_id, &todo_id).await {
+            Ok(todo) => {
+                match todo {
+                    Some(t) => Ok(Some(ToDoDto::from(&t))),
+                    None => Ok(None)
+                }
+            },
+            Err(e) => Err(e)
+        }
+        
     }
 
     async fn create_new_todo_group(&self, travel_id: &str, name: &str, tz: Option<isize>) -> Result<ToDoListGroupDto, TravelError> {
