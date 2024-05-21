4d91e438e5be59dab9737a2b562e7c7d84a14cd9
hirohata <hirohatatro@gmail.com>
2024-05-21 10:19:31 -0700

    feat: added create new todo function

--- backend/core-where-were-we/app/travel/src/use_cases/todo_use_cases.rs
+++ backend/core-where-were-we/app/travel/src/use_cases/todo_use_cases.rs
@@ -6,7 +6,6 @@ use crate::models::todo::entity::todo::Todo;
 use crate::models::todo::entity::todo_list_group::TodoListGroup;
 use crate::models::todo::id::todo_id::TodoId;
 use crate::models::todo::id::todo_list_group_id::TodoListGroupId;
-use crate::models::travel::entity::travel::Travel;
 use crate::models::travel::id::travel_id::TravelId;
 use crate::models::travel::id::user_id::UserId;
 use crate::service::todo_id_service::TodoIdService;
@@ -22,7 +21,7 @@ pub trait ToDoUseCases {
     /// The empty to-do is also created
     async fn create_new_todo_group(&self, user_id: &str, travel_id: &str, name: &str, tz: Option<i64>) -> Result<ToDoListGroupDto, TravelError>;
     /// create a new to-do
-    async fn crate_new_todo(&self, travel_id: &str, todo_list_group_id: &u32, summary: &str, description: Option<&str>, due_date: Option<&str>) -> Result<ToDoDto, TravelError>;
+    async fn crate_new_todo(&self, user_id: &str, travel_id: &str, todo_list_group_id: &u32, summary: &str, description: Option<&str>, due_date: Option<&str>) -> Result<ToDoDto, TravelError>;
     /// update a to-do list group
     async fn update_todo_list_group(&self, travel_id: &str, todo_list_group_id: &u32, name: &str, tz: Option<isize>) -> Result<ToDoDto, TravelError>;
     /// update a to-do
@@ -120,7 +119,7 @@ impl<R, RP, S> ToDoUseCases for TodoUseCaseInstractor<R, RP, S>
     async fn create_new_todo_group(&self, user_id: &str, travel_id: &str, name: &str, tz: Option<i64>) -> Result<ToDoListGroupDto, TravelError> {
         // check authentication
         self.check_authentication(user_id, travel_id).await?;
-        
+
         let travel_id = TravelId::try_from(travel_id)?;
 
         let latest_todo_list_group_id = self.todo_id_service.get_todo_list_group_id(&travel_id).await?;
@@ -138,8 +137,34 @@ impl<R, RP, S> ToDoUseCases for TodoUseCaseInstractor<R, RP, S>
         Ok(ToDoListGroupDto::from(&todo_list_group))
     }
 
-    async fn crate_new_todo(&self, travel_id: &str, todo_list_group_id: &u32, summary: &str, description: Option<&str>, due_date: Option<&str>) -> Result<ToDoDto, TravelError> {
-        todo!()
+    async fn crate_new_todo(&self, user_id: &str, travel_id: &str, todo_list_group_id: &u32, summary: &str, description: Option<&str>, due_date: Option<&str>) -> Result<ToDoDto, TravelError> {
+        // check authentication
+        self.check_authentication(user_id, travel_id).await?;
+
+        let travel_id = TravelId::try_from(travel_id)?;
+        let todo_list_group_id = TodoListGroupId::from(todo_list_group_id);
+
+        let latest_todo_id = self.todo_id_service.get_todo_id(&travel_id, &todo_list_group_id).await?;
+        let todo_id = TodoId::from(&latest_todo_id);
+
+        let due_date = match due_date {
+            None => None,
+            Some(d) => {
+                match chrono::DateTime::parse_from_rfc3339(d) {
+                    Ok(due) => {
+                        let timestamp = due.timestamp();
+                        Some(timestamp)
+                    },
+                    Err(_) => return Err(TravelError::UseCaseError("Invalid Datetime is provided".to_string()))
+                }
+            }
+        };
+
+        let todo = Todo::new(&todo_id, summary, description, due_date, None)?;
+        
+        self.todo_repository.save_todo(&travel_id, &todo_list_group_id, &todo).await?;
+
+        Ok(ToDoDto::from(&todo))
     }
 
     async fn update_todo_list_group(&self, travel_id: &str, todo_list_group_id: &u32, name: &str, tz: Option<isize>) -> Result<ToDoDto, TravelError> {
@@ -153,5 +178,5 @@ impl<R, RP, S> ToDoUseCases for TodoUseCaseInstractor<R, RP, S>
     async fn toggle_todo_done(&self, travel_id: &str, todo_list_group_id: &u32, todo_id: &u32) -> Result<ToDoDto, TravelError> {
         todo!()
     }
-    
+
 }
